20d98c7fdb1b03c99c3ccb0c20fa13e1da476758
vignesh <vigneshselvaraju1999@gmail.com>
2024-08-04 22:30:05 -0400

    pushing the code to github

---
+++ StockManagementApp-main/app/src/main/java/com/oguzhandurmaz/stockmanagementapp/model/Product.kt
@@ -0,0 +1,3 @@
+package com.oguzhandurmaz.stockmanagementapp.model
+// product model
+data class Product(val documentId: String, val barcode: String, val productBrand: String, val productName: String, val productImageUrl: String)
